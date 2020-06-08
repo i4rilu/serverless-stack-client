4808303fbd1c8aba85849fbc0944a948d78bdb58
Rimas Luksys <rluksys@gmail.com>
2020-06-08 06:28:40 -0700

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-east-1",
+        BUCKET: "rl-notes-app-uploads",
+    },
+    apiGateway: {
+        REGION: "us-east-1",
+        URL: "https://iwot0aphjl.execute-api.us-east-1.amazonaws.com/prod",
+    },
+    cognito: {
+        REGION: "us-east-1",
+        USER_POOL_ID: "us-east-1_Tigr2y8Uu",
+        APP_CLIENT_ID: "3u4plb1d6d30s7d8bf0cd3igdf",
+        IDENTITY_POOL_ID: "us-east-1:3e44831d-d7ef-439d-8813-c9cc95879363",
+    },
+};
